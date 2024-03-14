2079519d842abd1a1c2966f9266f18bf0ef36403
Vaibhav <vaibhavgp7436@gmail.com>
2024-03-14 08:16:28 +0530

    intial commit

---
+++ script.js
@@ -0,0 +1,25 @@
+console.log("Hello world");
+
+const accountID = 123456;
+let accountEmail = "vaibhav@google.com";
+var accountPassword = "23454";
+accountCity = "Jaipur";
+
+// accountID = 2  Not Allowed
+
+accountEmail = "vg@hc.com";
+accountPassword = "87867832";
+accountCity = "Bangalore";
+// console.log(accountID);
+
+// console.table([accountID, accountEmail, accountPassword, accountCity])
+
+const bignumber = 23232434343557677n
+
+console.log(typeof bignumber);
+
+// stack (Primitive) & heap (Non-Premitive)
+
+let name = "vaibhbhasd";
+
+
